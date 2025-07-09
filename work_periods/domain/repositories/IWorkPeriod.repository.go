008d691e5dbbf790d5ab8a2d6ff0adc8ede8a519
Alexis Guzman <spartanthelexvro@gmail.com>
2025-07-08 22:22:15 -0600

    Creando repositorio

--- work_periods/domain/repositories/IWorkPeriod.repository.go
+++ work_periods/domain/repositories/IWorkPeriod.repository.go
@@ -4,7 +4,8 @@ import "PyBot-DataServer/work_periods/domain/models"
 
 type IWorkPeriod interface {
 	CreatePeriod(wp models.WorkPeriod) (error, period_id int)
-	UpdatePeriod(end_hour string) (error) 
+	UpdatePeriod(end_hour string) (error)
+	GetDistanceAndWeight() (error, models.Reading) 
 	ReadingsRegister(r models.Reading) (error)
 	WasteCollectionRegister(wc models.WasteCollection) (error, id int)
 	WeightRegister(w models.WeightData) (error, id int)
