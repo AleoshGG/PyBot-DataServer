b54ed85032e3536ae58e138982b6ba693e43101c
Alexis Guzman <spartanthelexvro@gmail.com>
2025-07-11 00:05:12 -0600

    Terminando rutas para workPeriods

---
+++ work_periods/infrastructure/routes/workPeriods.routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"PyBot-DataServer/work_periods/infrastructure/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisteRouter(r *gin.Engine) {
+	workPeriodsRoutes := r.Group("/workPeriods")
+	{
+		workPeriodsRoutes.POST("/", controllers.NewCreatePeriodController().CreatePeriod)
+		workPeriodsRoutes.GET("/", controllers.NewGetDistanceAndWeightController().GetDistanceAndWeight)
+		workPeriodsRoutes.POST("/readings", controllers.NewReadingsRegisterController().ReadingsRegister)
+		workPeriodsRoutes.PATCH("/", controllers.NewUpdatePeriodController().UpdatePeriod)
+	}
+}
